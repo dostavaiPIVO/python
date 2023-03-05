b0884d92b0976e9142aaf7d353211add2f009cfb
movavi_school <andrejcepurnoj3@gmail.com>
2023-03-05 16:31:54 +0700

    05.03.23 lesson 22

---
+++ lesson 22/main.py
@@ -0,0 +1,56 @@
+import json
+# #  JSON. ЗАПИСЬ.
+# f = open("file.json", "w", encoding="utf-8")
+# # # f = open("file.txt", "w")
+# # # f.write("true")
+# peremennaya = [1, 4.3, [0,0], (0,0), None, True, "ENG", "РУС"]
+# json.dump(peremennaya, f, ensure_ascii=False)
+# f.close()
+#
+#
+# #  JSON. ЧТЕНИЕ.
+# f = open("file.json", "r", encoding="utf-8")
+# # print(f.read())
+# content = json.load(f)
+# print(content)
+# print(type(content))
+# f.close()
+
+
+# ЗАДАЧА 1
+# f = open("file.txt", "r", encoding="utf-8")
+# u = f.readlines()
+# f.close()
+# spic = {}
+# for i in u:
+#     p = i.split(": ")
+#     spic[p[0]] = p[1].rstrip()
+# print(spic)
+# o = open("file.json", "w", encoding="utf-8")
+# json.dump(spic, o, ensure_ascii=False)
+# o.close()
+
+
+#  ЗАДАЧА 2
+# f = open("file.json", "r")
+# sod = json.load(f)
+# f.close()
+# print(sod)
+# for indx, elem in enumerate(sod):
+#     if type(elem) == str:
+#         sod[indx] += "!"
+#     elif type(elem) in (int, float):
+#         sod[indx] += 1
+#     elif type(elem) == bool:
+#         sod[indx] = not elem
+#     elif type(elem) == list:
+#         sod[indx] += elem
+#     elif type(elem) == dict:
+#         sod[indx]["newkey"] = None
+# print(sod)
+
+
+import requests
+
+resp = requests.get(url="http://api.open-notify.org/iss-now.json").json()
+print(resp['iss_position'])
